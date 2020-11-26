a34e8501c0b363c209f19f1f449eeb8eccd53d4b
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-26 22:08:43 +0100

    add clock

---
+++ clock/src/lib.rs
@@ -0,0 +1,27 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let all_minutes = hours * 60 + minutes;
+        Clock {
+            hours: ((((all_minutes as f32 / 60.0).floor() as i32) % 24) + 24) % 24,
+            minutes: ((all_minutes % 60) + 60) % 60,
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
