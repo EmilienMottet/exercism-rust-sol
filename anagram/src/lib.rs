6307e801f984fa89612903eb339c874bec18304c
EmilienMottet <emilien.mottet@grenoble-inp.org>
2020-11-28 00:36:58 +0100

    optimize anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -3,15 +3,18 @@ use std::iter::FromIterator;
 use std::iter::Iterator;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let sorted_word = compute_str(word);
+    let word_lower = word.to_lowercase();
+    let sorted_word = compute_str(&word_lower);
     let filtered = possible_anagrams.into_iter().filter(|&&w| {
-        compute_str(&w).eq(&sorted_word) && !word.to_lowercase().eq(&w.to_lowercase())
+        let w_lower = &w.to_lowercase();
+        return compute_str(&w_lower).eq(&sorted_word)
+            && !word_lower.eq(w_lower);
     });
     filtered.cloned().collect()
 }
 
 fn compute_str(word: &str) -> String {
-    let mut sorted: Vec<char> = word.to_lowercase().chars().collect();
+    let mut sorted: Vec<char> = word.chars().collect();
     sorted.sort_by(|a, b| b.cmp(a));
     String::from_iter(sorted)
 }
